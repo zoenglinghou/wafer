3837a5ec526a3452c12928efe3a232585f3cea08
Linghao Zhang <linghao.zhang@protonmail.com>
2022-02-23 00:14:28 +0100

    First commit 2022-02-23

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+  description = "Manage nixpkgs master for all flakes";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs : {};
+}
