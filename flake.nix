4eb12083067c36b7ee80bbdec0f20197460fb9ad
Linghao Zhang <linghao.zhang@protonmail.com>
2022-02-23 00:20:13 +0100

    Follow nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Manage nixpkgs master for all flakes";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
   };
 
